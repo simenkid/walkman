544092267d974d52864f6025a97e91c6bf2b6f9c
Simen Li <simenkid@gmail.com>
2016-06-10 18:23:10 +0800

    api

--- api.js
+++ api.js
@@ -1,4 +1,6 @@
-var walkman = {};
+var walkman = {
+    audioContexts: null;
+};
 
 var Soundfont = require('soundfont-player');
 
@@ -6,9 +8,17 @@ var Soundfont = require('soundfont-player');
 // nameToUrl(name, format) String
 // noteToMidi(noteName) Integer
 
-walkman.createInstruments = function (names) {
+walkman.createInstrument = function (name, opts) {
+    var ac;
+    if (walkman.length > 5) {
+        // throw error
+    }
 
+    ac = ac || new AudioContext();
+    walkman.push(ac);
+    return Soundfont.instrument(ac, name, opts);
 };
+
 
 walkman.mapInstrumentToBeat = function (names) {
 
@@ -26,13 +36,13 @@ function Walkman(settings) {
     }
 }
 
-Walkman.prototype.loadSymbols = function (digits) {
+Walkman.prototype.loadSymbols = function (symbols) {
+    this.symbols = symbols;
+    this.createMusicFromSymbols(symbols);
 };
 
-Walkman.prototype.appendSymbols = function (digits) {
-};
-
-Walkman.prototype.clearSymbols = function (digits) {
+Walkman.prototype.clearSymbols = function () {
+    this.symbols = null;
 };
 
 Walkman.prototype.play = function () {
@@ -47,3 +57,24 @@ Walkman.prototype.stop = function () {
 Walkman.prototype.pause = function () {
 
 };
+
+// AudioContext
+// AudioContext.close()
+// AudioContext.createBuffer()
+// AudioContext.createBufferSource()
+// AudioContext.resume()
+// AudioContext.suspend()
+
+// Instrument
+// instrument(ac, name, options) => Promise
+
+
+// // SoundFont Player
+// player.play === player.start(name, when, options)
+// player.start(name, when, options) => AudioNode
+
+// player.stop(when, nodes) => Array
+// player.on(event, callback) => player
+// player.connect(destination) => AudioPlayer
+// player.schedule(when, events) => Array
+// player.listenToMidi(input, options) => player
